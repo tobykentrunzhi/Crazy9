e7593403e661af2ca7e70194004b21393a78a039
tobykentrunzhi <396367738@qq.com>
2015-10-27 19:15:31 +0800

    Initial Commit

---
+++ Crazy9/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Crazy9
+//
+//  Created by Arron on 10/27/15.
+//  Copyright (c) 2015 Arron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Crazy9/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Crazy9
+//
+//  Created by Arron on 10/27/15.
+//  Copyright (c) 2015 Arron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
